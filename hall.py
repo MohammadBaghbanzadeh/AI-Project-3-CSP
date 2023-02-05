535bf06214d0d5cf3d5712d7cfa41d66c823060d
ShervinEmrani <emranishervin@gmail.com>
2023-02-05 04:35:40 +0330

    hall class comments added

--- hall.py
+++ hall.py
@@ -1,6 +1,7 @@
 
 class Hall:
 
+    # # constructor with hall number and constraints as parameters
     def __init__(self, hall_number, hall_constraints):
             self.hall_number = hall_number
             self.hall_constraints = []
@@ -8,8 +9,10 @@ class Hall:
                 for constraint in hall_constraints:
                     self.hall_constraints.append(constraint)
 
+    # a function to add constraints to halls
     def add_constraint(self, constraint):
         self.hall_constraints.append(constraint)
 
+    # a function to show the constraints and the number of halls
     def __repr__(self):
         return f'Hall(hall_number={self.hall_number}, hall_constraints={self.hall_constraints})'
