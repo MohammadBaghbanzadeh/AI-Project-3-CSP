8214a71b476b883c784d498a19312cfe7123a23c
ShervinEmrani <emranishervin@gmail.com>
2023-02-05 04:36:07 +0330

    groups class comments added

--- groups.py
+++ groups.py
@@ -1,6 +1,7 @@
 
 class Groups:
 
+    # constructor with group number and preferences as parameters
     def __init__(self, group_number, group_preferences):
         if group_preferences is None:
             self.group_number = group_number
@@ -9,13 +10,10 @@ class Groups:
             self.group_number = group_number
             self.group_preferences = group_preferences
 
-    # def find_group(number, groups_list):
-    #     for i in groups_list:
-    #         if i.number == number:
-    #             return i
-
+    # a function to set the preferences of groups
     def set_preferences(self, preferences):
         self.group_preferences = preferences
 
+    # a function to show the preferences and the number of groups
     def __repr__(self):
         return f'Groups(group_number={self.group_number}, group_preferences={self.group_preferences})'
