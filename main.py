c650625e308faf894adbb9be776492a68b7d3300
ShervinEmrani <emranishervin@gmail.com>
2023-02-05 04:35:07 +0330

    comments added to main

--- main.py
+++ main.py
@@ -3,18 +3,19 @@ from hall import *
 from algo import *
 
 
+# a function to set number of halls
 def find_hall(number, halls_list):
     for hall in halls_list:
         if hall.hall_number == number:
             return hall
 
-
+# input of number of halls and groups
 halls, groups = input().split(" ")
 halls = int(halls)
 groups = int(groups)
 preferences_list = []
 
-
+# input of groups preferences
 for i in range(0, groups):
     preferences_list.append(input())
 
@@ -22,41 +23,38 @@ No_constraints = int(input())
 
 constraints_list = []
 
-
+# input of constraints
 for i in range(0, No_constraints):
     constraints_list.append(tuple([int(i) for i in input().split()]))
 
-halls_constraints = {}
 
 new_list = []
 
+# split the preferences list
 for i in preferences_list:
     new_list.append(i.split(" "))
 
 halls_list = []
 
+# create a list of halls and sets their number from 1 to number of halls
 for i in range(1, halls + 1):
     halls_list.append(Hall(i, None))
 
-# print(halls_list)
-
 groups_list = []
 
+# create a list of groups and sets their number from 1 to number of groups
 for i in range(1, groups + 1):
     groups_list.append(Groups(i, None))
 
 preferences_list_int = []
 
-# print(groups_list)
-
+# convert the preferences list to integers
 for i in new_list:
         int_list = [int(x) for x in i]
         preferences_list_int.append(int_list)
 
 
-# print(preferences_list_int)
-
-
+# set the domain of halls which is the preferences of groups
 for i in range(len(groups_list)):
     the_group = groups_list[i]
     the_preference = preferences_list_int[i]
@@ -65,34 +63,27 @@ for i in range(len(groups_list)):
         the_hall = find_hall(preference, halls_list)
         the_hall.add_constraint(the_group.group_number)
 
-
-# print(preferences_list)
-# # print(new_list)
-# # print(constraints_list)
-# print(halls_list[1].hall_constraints)
-# print(halls_list[2].hall_number)
-# print(halls_list)
-# print(groups_list)
+# printing the domain of groups which is the preferences of halls
 [print("domain of hall", i.hall_number, ":", i.hall_constraints) for i in halls_list]
-x = Algo(groups_list,constraints_list)
+x = Algo(groups_list, constraints_list)
 # MRV returns the most constrained variable
 mostConstrainedHall = x.MRV(halls_list)
-print(mostConstrainedHall.hall_number)
+# print(mostConstrainedHall.hall_number)
 # LCV returns a sorted list of groups according to the number of their preferences in descending order
 LCVOutput = x.LCV(mostConstrainedHall)
-print(LCVOutput)
+# print(LCVOutput)
 
 print(constraints_list)
 # give the ith LCVOutput as the assigned value
 newHallsList = x.forward_checking(halls_list, mostConstrainedHall, LCVOutput[0])
-solution = x.backtrack(halls_list, groups_list, constraints_list)
-if solution == False:
-    print("No")
-else:
-    print(solution)
+# solution = x.backtrack(halls_list, groups_list, constraints_list)
+# if solution == False:
+#     print("No")
+# else:
+#     print(solution)
 
 
-[print("domain of hall", i.hall_number, ":", i.hall_constraints) for i in newHallsList]
+[print("new domain of hall", i.hall_number, ":", i.hall_constraints) for i in newHallsList]
 
 
 
