7b21f7a9b02ff95462eab8eeafc762af667a1dfa
ShervinEmrani <emranishervin@gmail.com>
2023-02-05 04:15:50 +0330

    minor change to hall class

--- hall.py
+++ hall.py
@@ -2,20 +2,11 @@
 class Hall:
 
     def __init__(self, hall_number, hall_constraints):
-        if hall_constraints is None:
             self.hall_number = hall_number
             self.hall_constraints = []
-
-        else:
-            self.hall_number = hall_number
-            self.hall_constraints = []
-
-    # @classmethod
-    # def hall_number(cls):
-    #     return cls
-
-    # def hall_set_constraint(self, constraints):
-    #     self.hall_constraints = constraints
+            if hall_constraints is not None:
+                for constraint in hall_constraints:
+                    self.hall_constraints.append(constraint)
 
     def add_constraint(self, constraint):
         self.hall_constraints.append(constraint)
